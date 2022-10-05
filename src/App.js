c1b0b0592eb211363113222ad1e5626d2cfaaa80
ahmedzoha029 <57677449+ahmedzoha029@users.noreply.github.com>
2022-10-05 12:41:44 -0600

    Added Wrapper components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import FavoriteMeetups from "./pages/FavoriteMeetups";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 import MeetupsList from "./pages/MeetupsList";
 import NewMeetups from "./pages/NewMeetups";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<MeetupsList />}/>
         <Route path='/favorite-meetups' element={<FavoriteMeetups/>}/>
         <Route path='/new-meetups' element={<NewMeetups/>}/>
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,20 +1,23 @@
 import classes from "./MeetupItem.module.css";
+import Card from "../ui/Card"
 
 function MeetupItem({ address, content, description, image, title }) {
   return (
-    <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={image} alt={title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{title}</h3>
-        <address>{address}</address>
-        <p>{description}</p>
-      </div>
-      <div className={classes.action}>
-        <button>To Favorite</button>
-      </div>
-    </li>
+    <Card>
+      <li className={classes.item}>
+        <div className={classes.image}>
+          <img src={image} alt={title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <address>{address}</address>
+          <p>{description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorite</button>
+        </div>
+      </li>
+    </Card>
   );
 }
 
